5e7e436ef8cc7dde6f7dd125b17ae07d77456132
EDWINCSN <c.edwin32@hotmail.com>
2026-07-14 00:05:34 -0500

    feat(peliculas.controller): endpoint PUT para editar película

--- src/controllers/peliculas.controller.ts
+++ src/controllers/peliculas.controller.ts
@@ -1,5 +1,6 @@
 import type { Request, Response } from "express";
 import prisma from "../database/prisma.js";
+import multer from "multer";
 
 //ENDPOINT DELETE
 export const deleteMovie = async (req: Request, res: Response) => {
@@ -19,7 +20,7 @@ export const deleteMovie = async (req: Request, res: Response) => {
     await prisma.peliculas.delete({
       where: { id: Number(id) },
     });
-    res.status(200).json({ mensaje: "Pelicular eliminado" });
+    res.status(200).json({ Mensaje: "Pelicular eliminado" });
   } catch (error) {
     console.log({ error: error, mensaje: "error al eliminar perlicula" });
     res.status(500).json({ mensaje: "error al eliminar pelicular: " });
@@ -57,13 +58,64 @@ export const obtenerFotoId = async (req: Request, res: Response) => {
     if (!pelicula) {
       return res.status(404).json({ mensaje: "pelicula no encontrado" });
     }
-    if (pelicula.mime_type===null || pelicula.foto===null) {
+    if (pelicula.mime_type === null || pelicula.foto === null) {
       return res.status(404).json({ mensaje: "formato imagen no soportada" });
     }
     res.setHeader("Content-Type", pelicula.mime_type);
     res.send(pelicula.foto);
   } catch (error) {
     console.log({ error: error, mensaje: "error al obtner foto peliculas" });
-    res.status(500).json({ mensaje: "error al obtenr foto peliculas " });
+    res.status(500).json({ Mensaje: "error al obtenr foto peliculas " });
   }
 };
+
+//ENDPOINT EDITAR PELICULA
+export const modificarMovieId = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const { genero, sinopis, titulo } = req.body; //nuevos datos
+    const archivo = req.file;
+
+
+    const pelicula = await prisma.peliculas.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+
+    if (!pelicula) {
+      return res
+        .status(404)
+        .json({ Mensaje: "pelicula no encontrado para modificar" });
+    }
+
+    let bufferFinal: Uint8Array | null;
+    let mimeTypeFinal: string | null;
+
+    if (!archivo) {
+      // No mandaron foto nueva -> conservar la existente
+      bufferFinal = pelicula.foto;
+      mimeTypeFinal = pelicula.mime_type;
+    } else {
+      // Mandaron foto nueva -> usar la del archivo subido
+      bufferFinal = archivo.buffer;
+      mimeTypeFinal = archivo.mimetype;
+    }
+
+    const nuevaMovie = await prisma.peliculas.update({
+      where: { id: Number(id) },
+      data: {
+        genero,
+        sinopis,
+        titulo,
+        mime_type: mimeTypeFinal,
+        foto: bufferFinal
+      },
+    });
+    res.status(200).json({ Mensaje: "Pelicula modificada con exito" });
+  } catch (error) {
+    console.log({ error: error, mensaje: "error al editar  peliculas" });
+    res.status(500).json({ Mensaje: "error al editar  peliculas " });
+  }
+};
+
