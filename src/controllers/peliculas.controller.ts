b0553c39ae741d0fc80036c6c87b1f5bac237111
EDWINCSN <c.edwin32@hotmail.com>
2026-07-13 22:55:32 -0500

    feat(peliculas.controller): add endpoint listarMovies, obtenerFotoId

--- src/controllers/peliculas.controller.ts
+++ src/controllers/peliculas.controller.ts
@@ -1,28 +1,69 @@
-import type { Request, Response  } from "express";
+import type { Request, Response } from "express";
 import prisma from "../database/prisma.js";
 
 //ENDPOINT DELETE
-export const deleteMovie=async(req:Request, res:Response)=>{
+export const deleteMovie = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
 
-    try {
-        const {id}=req.params;
+    const existeMovie = await prisma.peliculas.findUnique({
+      where: { id: Number(id) },
+    });
 
-        const existeMovie=await prisma.peliculas.findUnique({
-            where:{id:Number(id)}
-        })
+    if (!existeMovie) {
+      return res
+        .status(404)
+        .json({ mensaje: "No existe pelicula para eliminar" });
+    }
 
-        if(!existeMovie){
-            return res.status(404).json({mensaje:"No existe pelicula para eliminar"})
-        }
+    await prisma.peliculas.delete({
+      where: { id: Number(id) },
+    });
+    res.status(200).json({ mensaje: "Pelicular eliminado" });
+  } catch (error) {
+    console.log({ error: error, mensaje: "error al eliminar perlicula" });
+    res.status(500).json({ mensaje: "error al eliminar pelicular: " });
+  }
+};
 
-        await prisma.peliculas.delete({
-            where:{id:Number(id)}
-        })
-        res.status(200).json({mensaje:"Pelicular eliminado"})
-        
-    } catch (error) {
-        console.log({error:error, mensaje:"error al eliminar perlicula"})
-        res.status(500).json({mensaje:"error al eliminar pelicular: "})        
-    }   
-    
-}
+//ENDPOINT LISTAR
+export const listarMovies = async (req: Request, res: Response) => {
+  try {
+    const peliculas = await prisma.peliculas.findMany({
+      select: {
+        id: true,
+        genero: true,
+        mime_type: true,
+        sinopis: true,
+        titulo: true,
+      },
+    });
+    res.status(200).json(peliculas);
+  } catch (error) {
+    console.log({ error: error, mensaje: "error al listar peliculas" });
+    res.status(500).json({ mensaje: "error al listar peliculas " });
+  }
+};
+
+//ENDPOINT OBTNER FOTO VEHICULO
+export const obtenerFotoId = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+    const pelicula = await prisma.peliculas.findUnique({
+      where: {
+        id: Number(id),
+      },
+    });
+    if (!pelicula) {
+      return res.status(404).json({ mensaje: "pelicula no encontrado" });
+    }
+    if (pelicula.mime_type===null || pelicula.foto===null) {
+      return res.status(404).json({ mensaje: "formato imagen no soportada" });
+    }
+    res.setHeader("Content-Type", pelicula.mime_type);
+    res.send(pelicula.foto);
+  } catch (error) {
+    console.log({ error: error, mensaje: "error al obtner foto peliculas" });
+    res.status(500).json({ mensaje: "error al obtenr foto peliculas " });
+  }
+};
