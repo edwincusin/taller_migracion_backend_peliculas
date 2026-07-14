27300e3c3ce9239c6154cb6e749de3975bd46117
EDWINCSN <c.edwin32@hotmail.com>
2026-07-14 01:03:38 -0500

    feat(server): configurar index.ts y corregir script dev con tsx

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express'
+import cors from 'cors'
+
+import { routerLogin } from './routes/route.auth.js'
+import { routerPeliculas } from './routes/route.peliculas.js'
+
+const app=express();
+
+const PORT=3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/auth",routerLogin);
+app.use("/auth",routerPeliculas);
+
+app.listen(PORT,()=>{
+    console.log("SERVICIO INICIADO PORT: "+PORT)
+})
