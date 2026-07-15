65226972fc6bb1844def56606ee597d667fe09b1
EDWINCSN <c.edwin32@hotmail.com>
2026-07-14 22:50:39 -0500

    feat(index.ts): conectar Swagger UI en /api-docs

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import cors from 'cors'
 import { routerLogin } from './routes/route.auth.js'
 import { routerPeliculas } from './routes/route.peliculas.js'
 
+import { swaggerDocs } from './swagger.js'
+
+
 const app=express();
 
 const PORT=3000;
@@ -13,7 +16,9 @@ app.use(express.json());
 
 app.use("/auth",routerLogin);
 app.use("/auth",routerPeliculas);
+swaggerDocs(app,PORT);
 
 app.listen(PORT,()=>{
-    console.log("SERVICIO INICIADO PORT: "+PORT)
+    console.log("SERVICIO INICIADO PORT: "+PORT);
+    
 })
