38745eec21722be67380da09511f77298cce3916
EDWINCSN <c.edwin32@hotmail.com>
2026-07-14 23:28:01 -0500

    docs(peliculas): add swagger -documentar GET /auth/peliculas

--- src/routes/route.peliculas.ts
+++ src/routes/route.peliculas.ts
@@ -7,7 +7,24 @@ export const routerPeliculas=Router();
 
 const upload=multer({storage:multer.memoryStorage()});
 
+
+
+/**
+ * @swagger
+ * /auth/peliculas:
+ *  get:
+ *      summary: Obtiene toda la lista completa de las peliculas.
+ *      tags: [peliculas]
+ *      security:
+ *          - bearerAuth: []
+ *      responses:
+ *          200:
+ *              description: Lista de todas las peliculas.
+ *          500:
+ *              description: Error al listar peliculas.
+ */
 routerPeliculas.get("/peliculas",validarAuth,listarMovies);
+
 routerPeliculas.get("/peliculas/:id/foto",validarAuth,obtenerFotoId);
 routerPeliculas.post("/peliculas",validarAuth,upload.single("file"),crearMovie);
 routerPeliculas.put("/peliculas/:id",validarAuth,upload.single("file"),modificarMovieId);
